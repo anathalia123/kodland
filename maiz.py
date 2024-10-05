6a8ac1817c3cd706c26fb882bda238b3da1e039e
anathalia123 <anathaliaariasrosero@gmail.com>
2024-10-05 16:47:52 -0500

    Create maiz.py

---
+++ maiz.py
@@ -0,0 +1,13 @@
+traducion= {
+    "JUICIOSO":"persona responsable o que cumple con sus deberes"
+    "CANGUIL":"palomitas de maiz"
+    "AGUACATE":"palta"
+}
+
+palabra= input("La palabra que quieras (con preferencia en mayuscula)")
+
+
+if palabras in traducion.keys():
+    print("sus significado es:", traduccion[traduccion])
+else:
+    print("esa palabra no esta disponible")
